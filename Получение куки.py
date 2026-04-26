210e50e07b63c8549764934fc90847670b57f8c7
user <schtschepetkovkivl@gmail.com>
2026-04-26 23:09:03 +0300

    1

--- Получение куки.py
+++ Получение куки.py
@@ -1,7 +1,7 @@
 import json
 from selenium import webdriver
 driver = webdriver.Chrome()
-driver.get("https://staging.sso.strana.com/realms/strana/protocol/openid-connect/auth?client_id=partners&redirect_uri=https%3A%2F%2Fstagingpartner.strana.com%2Flk%2Ftrading&state=c2c6de16-83ef-4f2d-849a-a6668506a1dc&response_mode=fragment&response_type=code&scope=openid&nonce=b3f05b31-faff-4d14-ba8c-f1a7e652381e&code_challenge=4TIkmuTii1ex6q3mJW-86Wz2alwEdsaX_GvhZKaNMt8&code_challenge_method=S256")
+driver.get("https://stagingpartner.strana.com/lk/trading")
 
 input()
 
