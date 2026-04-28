a3aef32e1115d2cd8975b1aea46fc1c8b3bbd30d
user <schtschepetkovkivl@gmail.com>
2026-04-28 21:30:05 +0300

    1

--- Fixture homework/conftest.py
+++ Fixture homework/conftest.py
@@ -1,8 +1,18 @@
-import os
-import pytest
-from selenium import webdriver
+import os # импорт модуля для работы с ос (переменные окружения, файлы-пути)
+import pytest # импорт библиотеки для фикстур
+from selenium import webdriver # импорт вебдрайвера
 from selenium.webdriver.chrome.options import Options
-
+# Костыль
+options = webdriver.ChromeOptions()
+options.add_argument("--incognito")
+options.add_argument("--disable-blink-features=AutomationControlled")  # убирает флаг автоматизации
+options.add_experimental_option("excludeSwitches", ["enable-automation"])  # убирает баннер "controlled by automated software"
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False
+    }
+)
 
 @pytest.fixture()
 def driver(request):
@@ -12,3 +22,13 @@ def driver(request):
     request.cls.driver = driver
     yield
     driver.quit()
+
+@pytest.fixture(autouse=True)
+def setup_environment_properties():
+    properties = {
+        "STAGE": os.environ["STAGE"],
+        "BROWSER": os.environ["BROWSER"]
+    }
+    with open("allure-results/environment.properties", "w") as file:
+        for key, value in properties.items():
+            file.write(f"{key}={value}\n")
--- Fixture homework/test_example.py
+++ Fixture homework/test_example.py
@@ -6,13 +6,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from allure_commons.types import Severity
 
-
+# pytest "Fixture homework/test_example.py" - запуск в терминале
+# allure serve allure-results - получение отчета
+# $env:STAGE="Stage 1.AQA";$env:BROWSER="Chrome";pytest "Fixture homework/test_example.py" - запуск с окружением
 
 @allure.epic("Accounts")
 @allure.feature("Login")
 @allure.story("Pages")
 @pytest.mark.usefixtures("driver")
-class TestPages:
+class TestAuth:
 
 
     @pytest.mark.smoke
@@ -25,6 +27,12 @@ class TestPages:
 
         with allure.step("Assert open page. Step 2"):
             assert self.driver.current_url == "https://demoqa.com/login", "Ошибка ULR страницы входа"
+            allure.attach(
+                body = self.driver.get_screenshot_as_png(),
+                name = "Login page",
+                attachment_type= allure.attachment_type.PNG
+
+            )
 
     @pytest.mark.regression
     @allure.title("Open book page")
@@ -36,6 +44,12 @@ class TestPages:
 
         with allure.step("Assert open books page. Step 4"):
             assert self.driver.current_url == "https://demoqa.com/books", "Ошибка ULR страницы с книгами"
+            allure.attach(
+                body=self.driver.get_screenshot_as_png(),
+                name="books page",
+                attachment_type=allure.attachment_type.PNG
+
+            )
 
     @pytest.mark.profile
     @allure.title("Open profile page")
@@ -44,3 +58,9 @@ class TestPages:
     def test_open_profile_page(self):
         self.driver.get("https://demoqa.com/profile")
         assert self.driver.current_url == "https://demoqa.com/profile", "Ошибка ULR страницы профиля"
+        allure.attach(
+            body=self.driver.get_screenshot_as_png(),
+            name="Profile page",
+            attachment_type=allure.attachment_type.PNG
+
+        )
